e50d3a56657416c25d2fa19e68591b1e2bfb3490
cuongnh <cuongnh@toprate.io>
2023-07-19 09:05:30 +0700

    ignore connect db

--- src/main/java/com/example/springbootcrud/repository/ChuyenXeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootcrud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.springbootcrud.model.ChuyenXe;
-
-@Repository
-public interface ChuyenXeRepository extends JpaRepository<ChuyenXe, Long> {
-}
